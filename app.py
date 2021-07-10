278f2d8183510c303bf3fae18b8fb0fab745eac5
francosbenitez <francosbenitez@gmail.com>
2021-07-10 14:38:18 -0300

    clean up the code a little

--- app.py
+++ app.py
@@ -1,23 +1,23 @@
-from flask import Flask                             # import Flask
-from flask import render_template, request                   # render for templates & request to bring data from the user
-from flaskext.mysql import MySQL                    # connect to database
+from flask import Flask                             
+from flask import render_template, request                   
+from flaskext.mysql import MySQL                    
 
-app = Flask(__name__)                               # create the application
-mysql = MySQL()                                     # connecting to the database
+app = Flask(__name__)                               
+mysql = MySQL()                                     
 app.config['MYSQL_DATABASE_HOST']='localhost'
 app.config['MYSQL_DATABASE_USER']='root'
 app.config['MYSQL_DATABASE_PASSWORD']='1234'
 app.config['MYSQL_DATABASE_BD']='sistema'
-mysql.init_app(app)                                 # create the connection 
+mysql.init_app(app)                                 
 
-@app.route("/")                                     # routing so that user enters root
+@app.route("/")                                     
 def index():
     sql="INSERT INTO `sistema`.`pacientes` (`id`, `nombre`, `correo`, `foto`) VALUES (NULL, 'Juan Pablo', 'juanpablo@gmail.com', 'juanpablo.jpg');"
-    conn=mysql.connect()                            # connect to mysql.init_app(app)
-    cursor=conn.cursor()                            # store what runs
-    cursor.execute(sql)                             # run the SQL statement
-    conn.commit()                                   # close the connection
-    return render_template("pacientes/index.html")  # identify the folder and the html file
+    conn=mysql.connect()                            
+    cursor=conn.cursor()                            
+    cursor.execute(sql)                             
+    conn.commit()                                   
+    return render_template("pacientes/index.html")  
 
 @app.route('/create') 
 def create(): 
@@ -32,30 +32,10 @@ def storage():
     datos=(_nombre,_correo,_foto.filename)
     conn = mysql.connect()
     cursor = conn.cursor()
-    cursor.execute(sql, datos)                      # join the sql and datos variables 
+    cursor.execute(sql, datos)                      
     conn.commit()
     return render_template("pacientes/index.html")
 
 if __name__=="__main__":                             
-    app.run(debug=True)                             # set debug mode
+    app.run(debug=True)                             
 
-#mysql=MySQL()
-#app.config["MYSQL_DATABASE_HOST"]="localhost"
-#app.config["MYSQL_DATABASE_USER"]="root"
-#app.config["MYSQL_DATABASE_PASSWORD"]=""
-#app.config["MYSQL_DATABASE_DB"]="localhost"
-#mysql.init_app(app)
-
-# @app.route("/")
-#def index():
-#    sql="INSERT INTO"
-#    conn=mysql.connect()
-#    cursor=conn.cursor()
-#    cursor.execute(sql)
-#    conn.commit()
-    
-#    return render_template("empleados/index.html")
-
-#if __name__=="__main__":
-#    app.run(debug=True)
-    
