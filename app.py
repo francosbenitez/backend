b526bebed32fb74176a615404b76fcc0f28e9469
francosbenitez <francosbenitez@gmail.com>
2021-07-11 09:37:53 -0300

    update

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask                             
-from flask import render_template, request, redirect                   
+from flask import render_template, request, redirect, url_for                   
 from flaskext.mysql import MySQL                    
 from datetime import datetime
 import os 
